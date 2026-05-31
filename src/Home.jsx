e770cbab6bbbe40a60510116d129a6a3ea5cd330
leejun05 <159256202+leejun05@users.noreply.github.com>
2026-05-31 22:40:52 +0900

    Delete src directory

--- src/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-        <div style={{ margin: 10 }}>
-            <h2>Home</h2>
-            <p>안녕하세요~</p>
-        </div>
-    )
-}
--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import NumberList from "./NumberList";
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <NumberList numbers={[1,2,3,4,5]}/>
-    </React.StrictMode>
-);
